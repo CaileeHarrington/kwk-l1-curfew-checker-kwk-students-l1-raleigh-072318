ee8e0ebba6bd30e09bdee244ec527acd40dc4174
CaileeHarrington <cailee.harrington@gmail.com>
2018-07-24 18:44:43 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,29 @@
 def simple_curfew_checker(time)
-  # code goes here
+  If time < 11 
+    puts "You've got time."
 end
 
 def curfew_checker(time)
-  # code goes here
+  If time < 11 && > 10
+    puts "You've got a little time."
 end
 
 def complex_curfew_checker(time)
-  # code goes here
+  If time < 10:45 && > 10:30
+    puts "You've got approximately 15 to 30 minutes. Get moving!"
+  else time < 10:30 
+    puts "You've got over thirty minutes. Relax."
 end
 
 def deluxe_curfew_checker(time)
-  # code goes here
+  If time < 10:59 && > 10:45
+    puts "You've got less than 15 minutes."
+  Else time < 10:45 && > 10:30 
+    puts "You've got approximately 15 to 30 mimutes. Get moving!"
+  Else time < 10:30
+    puts "You've got over 30 minutes."
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
+  If time < 10:59, 10:59 - currenty_time
 end
